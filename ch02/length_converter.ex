5d2977c6db5ab8040a87c6f908f56de25e8fef89
J C <jc@b.local>
2024-03-19 10:07:38 -0400

    Function Clauses

--- ch02/length_converter.ex
+++ ch02/length_converter.ex
@@ -1,11 +1,9 @@
 defmodule MeterToLengthConverter.Feet do
-  def convert(m) do
+  def convert(:feet, m) do
     m * 3.28084
   end
-end
 
-defmodule MeterToLengthConverter.Inch do
-  def convert(m) do
+  def convert(:inch, m) do
     m * 39.3701
   end
 end
