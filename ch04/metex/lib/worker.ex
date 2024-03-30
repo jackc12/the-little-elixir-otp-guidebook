9c85f3a6eecc7974590d852f41cf7de674f970d1
jackc12 <jc@me.com>
2024-03-30 12:43:56 +0100

    Listing 4.2 - 4.7. Do not have internet so not using HTTPoison.

---
+++ ch04/metex/lib/worker.ex
@@ -0,0 +1,76 @@
+defmodule Metex.Worker do
+  use GenServer
+
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  ## Server API
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}°C", new_stats}
+      _ ->
+        {:reply, :error, stats}
+    end
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  ## Helper Functions
+  def temperature_of(location) do
+    # url_for(location) |> HTTPoison.get() |> parse_response
+    url_for(location) |> no_internet |> parse_response
+  end
+
+  defp no_internet(url) do
+    {:ok, %HTTPoison.Response{body: "{\"main\": {\"temp\": 273.15}}", status_code: 200}}
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode!() |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apikey do
+    "e5ec6d98d830a819838931aeaced0720"
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
+  end
+end
