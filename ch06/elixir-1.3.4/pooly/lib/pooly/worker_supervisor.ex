5acd571be4cf694d52eb9b17b14e6f7a80543a48
jackc12 <jc@me.com>
2024-04-02 13:35:25 +0200

    Listing 6.3 Worker used to test Pooly (lib/pooly/sample_worker.ex).

--- ch06/elixir-1.3.4/pooly/lib/pooly/worker_supervisor.ex
+++ ch06/elixir-1.3.4/pooly/lib/pooly/worker_supervisor.ex
@@ -14,5 +14,13 @@ defmodule Pooly.WorkerSupervisor do
   #############
 
   def init({m,f,a}) do
+    worker_opts = [restart: :permanent, function: f]
+
+    children = [worker(m, a, worker_opts)]
+
+    opts = [strategy: :simple_one_for_one,
+            max_restarts: 5,
+            max_seconds: 5]
+    supervise(children, opts)
   end
 end
